064e20429cf4214ee2a57c22185c6c63fbc2cd4b
Aronic <aronic.tekkuniku@example.com>
2025-04-17 10:31:16 +0800

    Add Descriptions in the prototype + also enabled raw strings

--- lib/constants/book_prototype.dart
+++ lib/constants/book_prototype.dart
@@ -23,6 +23,22 @@ final Book makeine = Book(
   themes: ["School Life"],
   format: BookFormat.webnovel,
   chapters: 21,
+  description: r"""
+“Huh? Who are you calling losing?”
+
+Background character Nukumizu Kazuhiko witnesses a popular girl, Yanami Anna, getting rejected by another guy.
+
+“Even though he said we'll get married. Don't you think he's pretty mean?”
+“When did he say that?”
+“When we were 4 or 5 years old.”
+
+Nukumizu has no choice but to comfort Yanami, but this leads him to get involved with other losing heroines like Yakishio Lemon from the Track and Field Club and Chika Komari from the Literature Club.
+
+“Nukumizu-kun. There are two types of girls. Childhood friends or cheating cats.”
+“I see. Well, that's a bold statement.”
+
+Bless these girls who only shine after they lose.
+  """
 );
 
 final Book theNovelsExtra = Book(
@@ -35,6 +51,13 @@ final Book theNovelsExtra = Book(
   themes: ["Demons", "Magic", "Monsters", "Reincarnation", "School Life"],
   format: BookFormat.webtoon,
   chapters: 124,
+  description: r"""
+Wouldn’t it be a dream to wake up in a world of your own creation? For Kim Hajin, it’s nothing short of a nightmare.
+
+With no clue how or why he’s been transported into his web novel — as a mere extra, no less! — Hajin has little choice but to see his story through until the end. Lucky for him, as the author, he knows this perilous world like the back of his hand.
+
+But when discrepancies in the plot begin popping up left and right, Hajin soon realizes that things aren’t quite so simple, after all…
+"""
 );
 
 final Book brainrotGF = Book(
@@ -46,6 +69,10 @@ final Book brainrotGF = Book(
   genres: ["Comedy", "Romance", "Slice Of Life"],
   format: BookFormat.manga,
   chapters: 19,
+  description: r"""
+She's cute! She's beautiful! She's sexy! She... speaks in tongues? Meet the Brainrot Girlfriend! She means well, but good luck understanding what she says.
+Brainrot translation: shes skibidi shes toilet shes roblox she girl math goat bussing this better caul saul mrbeast metal pipe falling the roblox amogus she gigachad glazer but colleen ballinger nonchalant poppy playtime what she roblox L rn
+"""
 );
 
 final Book theFragrantFlowerBloomsWithDignity = Book(
@@ -58,6 +85,9 @@ final Book theFragrantFlowerBloomsWithDignity = Book(
   themes: ["School Life"],
   format: BookFormat.manga,
   chapters: 147,
+  description: """
+In a certain place, there are two neighboring high schools. Chidori High School, a bottom-feeder boys' school where idiots gather, and Kikyo Girls' School, a well-established girls' school. Rintaro Tsumugi, a strong and quiet second year student at Chidori High School, meets Kaoruko Waguri, a girl who comes as a customer while helping out at his family's cake shop. Rintaro feels comfortable spending time with Kaoruko, but she is a student at Kikyo Girls, a neighboring school that thoroughly dislikes Chidori High.
+"""
 );
 
 final Book threeSixtyFiveDaysToTheWedding = Book(
@@ -70,6 +100,7 @@ final Book threeSixtyFiveDaysToTheWedding = Book(
   themes: ["Office Workers"],
   format: BookFormat.manga,
   chapters: 110,
+  description: "Takuya and Rika are co-workers for a travel agency and have barely ever interacted as they are both introverted, anti-social people. Both are doing their jobs as normal when an announcement from the higher-ups comes down: in one year, an overseas branch will be opening in Siberia, and an employee from their location will be sent to work there! While there's no telling who will be chosen to go, it is explained that people who are married and have families are far less likely to be sent! Neither Takuya nor Rika want to go to Siberia! Desperate to protect their solitary lives, they develop a plan: pretend that they are getting married to each other! Will they be able to keep up this charade for 365 days, and, maybe, even fall for one another in that time?"
 );
 
 final Book farmingLifeInAnotherWorld = Book(
@@ -82,6 +113,7 @@ final Book farmingLifeInAnotherWorld = Book(
   themes: ["Harem", "Magic", "Monster Girls", "Survival"],
   format: BookFormat.manga,
   chapters: 291,
+  description: "Having died of sickness at the age of 39, Machio Hiraku is given the opportunity to go to another world. When asked what his wish is by God intending to give him strength, he requests to have a healthy body, offered a second wish, he chooses to live peacefully, a third wish to be able to know the local language, and for a final wish, to be able to be a farmer."
 );
 
 final Book theExtrasAcademySurvivalGuide = Book(
@@ -94,6 +126,13 @@ final Book theExtrasAcademySurvivalGuide = Book(
   themes: ["Harem", "Magic", "Reincarnation", "Slice of Life", "Supernatural", "Time Travel"],
   format: BookFormat.webtoon,
   chapters: 73,
+  description: r"""
+Ed Rothstaylor is a third-rate villain in a game, disowned by his family and kicked out of the dormitory for his misdeeds. One day, our main character wakes up as this very Ed, and realizes he must earn a scholarship so he can graduate and be set for life.
+
+Now living in the woods, he resolves to keep away from the protagonists, but four of the female leads just won't leave him alone. Not only that, his presence starts to change the timeline and events of the game!
+
+Can Ed help the story progress while staying out of the limelight?
+"""
 );
 
 final Book soloLeveling = Book(
@@ -106,6 +145,7 @@ final Book soloLeveling = Book(
   themes: ["Magic", "Monsters", "Supernatural"],
   format: BookFormat.webtoon,
   chapters: 200,
+  description: "10 years ago, after “the Gate” that connected the real world with the monster world opened, some of the ordinary, everyday people received the power to hunt monsters within the Gate. They are known as “Hunters”. However, not all Hunters are powerful. My name is Sung Jin-Woo, an E-rank Hunter. I’m someone who has to risk his life in the lowliest of dungeons, the “World’s Weakest”. Having no skills whatsoever to display, I barely earned the required money by fighting in low-leveled dungeons… at least until I found a hidden dungeon with the hardest difficulty within the D-rank dungeons! In the end, as I was accepting death, I suddenly received a strange power, a quest log that only I could see, a secret to leveling up that only I know about! If I trained in accordance with my quests and hunted monsters, my level would rise. Changing from the weakest Hunter to the strongest S-rank Hunter!"
 );
 
 final Book windBreaker = Book(
@@ -118,6 +158,7 @@ final Book windBreaker = Book(
   themes: ["Delinquents", "School Life"],
   format: BookFormat.webtoon,
   chapters: 153,
+  description: "Jo Ja-Hyun (Jay) is the student president of Taeyang (Sunny) High. He is not just a smart student but an extreme biker with high techniques. As you follow his bike, you will meet his friends, love, and adventures."
 );
 
 final Book myOlderSistersFriend = Book(
@@ -130,6 +171,7 @@ final Book myOlderSistersFriend = Book(
   themes: ["Gyaru", "School Life"],
   format: BookFormat.manga,
   chapters: 17,
+  description: "Hinako, a friend of Asahi's older sister, is a frequent guest at the home of the high school boy. She always misleads his mind with her suggestive attitude while treating him like a little brother... or rather, pet?"
 );
 
 
